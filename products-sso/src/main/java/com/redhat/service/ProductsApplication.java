42b22df13f140963e654504de679f9496bc7d7f0
pszuster <pszuster@redhat.com>
2017-06-21 16:31:14 -0300

    deleted prods-sso

--- products-sso/src/main/java/com/redhat/service/ProductsApplication.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class ProductsApplication extends Application {
-	
-}
