fd948527595d93f52e8d736e85b871b517a6e02c
pszuster <pszuster@redhat.com>
2017-06-28 19:14:23 -0300

    body-parser

--- 3scale-cli-pablo/server.js
+++ 3scale-cli-pablo/server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
     app     = express(),
+    bodyParser = require('body-parser'),
     swagger = require("./lib/swagger");
 
 app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.get('/', function (req, res) {
