0d159f20b852d403f04e09d8b56c8bfe54d09a80
pszuster <pszuster@redhat.com>
2017-06-28 17:20:29 -0300

    server.js

--- 3scale-cli-pablo/server.js
+++ 3scale-cli-pablo/server.js
@@ -7,12 +7,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.get('/', function (req, res) {
 	res.render('index.html');
-}
+});
 app.post('/import', function(req,res){
 
 	console.log("Received: " + req.body.access_token);
- swagger.import(options.file, options.service, options.appplan, options.pattern)
-}
+ swagger.import(options.file, options.service, options.appplan, options.pattern);
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
